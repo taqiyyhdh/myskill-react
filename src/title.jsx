83f2a99cde1fe41fbb629634979ca16cd4497ee6
taqiyyhdh <taqiyyhdh@gmail.com>
2026-06-12 17:10:47 +0700

    state

--- src/title.jsx
+++ src/title.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
-function Title () {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <h3>Welcome to React</h3>
-    </div>
-  );
+class Title extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Hello World",
+      subTitle: "Welcome to React",
+    };
+  }
+  changeTitle = () => {
+    this.setState({
+      title: "Hello Taqi",
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <h3>{this.state.subTitle}</h3>
+        <button onClick={this.changeTitle}>Change Title</button>
+      </div>
+    );
+  }
 }
 
 export default Title;
