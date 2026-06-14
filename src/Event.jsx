5d994b802517d2fd8a4b4993fb42492688071990
taqiyyhdh <taqiyyhdh@gmail.com>
2026-06-14 15:16:49 +0700

    argument on event

--- src/Event.jsx
+++ src/Event.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 function FootBall () {
-  const shoot = () => {
-    alert("Great Shot!");
+  const shoot = (a) => {
+    alert(a);
   }
   return (
-    <button onClick={shoot}>Take the shot!</button>
+    <button onClick={() => shoot("Goal!")}>Take the shot!</button>
   );
 }
 export default FootBall;
