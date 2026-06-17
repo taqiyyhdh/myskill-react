f04f68b7e1888dfa58634247369b77accb5e58c0
taqiyyhdh <taqiyyhdh@gmail.com>
2026-06-17 19:26:12 +0700

    react router 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
 import Layout from './pages/Layout'
 import Home from './pages/Home'
 import Blogs from './pages/Blogs'
 import Contact from './pages/Contact'
 import NoPage from './pages/NoPage'
-function App() {
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: 'blogs',
+        element: <Blogs />
+      },
+      {
+        path: 'contact',
+        element: <Contact />
+      },
+      {
+        path: '*',
+        element: <NoPage />
+      },
+    ],
+  },
+]);
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App
